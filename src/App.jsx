1130a5cc51b7289bde06188ea55c423d2403787e
Pratham <122166099+pratham40@users.noreply.github.com>
2024-08-17 17:51:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
