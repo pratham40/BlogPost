11f8f59f8ca323cf8921e3e7bffe9473cdc24460
Pratham <122166099+pratham40@users.noreply.github.com>
2024-08-18 14:42:50 +0530

    auth

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,5 +1,5 @@
 
-export const config={
+export const conf={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
